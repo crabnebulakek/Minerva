b120a2fc8ef872393f6433d8c29cc9236217439f
Harpocrates <crabnebulakek@gmail.com>
2017-11-18 18:39:37 -0500

    Quick Test Changes

--- BigBrother/BigBrother/Program.cs
+++ BigBrother/BigBrother/Program.cs
@@ -18,9 +18,12 @@ namespace BigBrother
             _client = new DiscordSocketClient();
             await _client.LoginAsync(TokenType.Bot, "MzgxMjU5ODQwMjU4OTY1NTA0.DPJDKw.v3b5tHCdy_9BeC7VKfSbQNxEQBc");
             await _client.StartAsync();
+            Console.WriteLine("Logged in and started.");
             await Task.Delay(15000);
+            Console.WriteLine("Logging out and stopping.");
             await _client.StopAsync();
             await _client.LogoutAsync();
+            await Task.Delay(3000);
         }
 
     }
