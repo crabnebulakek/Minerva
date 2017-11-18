f0917b00a480179b78dd91b25501feb7d36d4629
HarpocratesM <crabnebulakek@gmail.com>
2017-11-18 14:52:01 -0800

    Initial Commit

---
+++ BigBrother/BigBrother/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace BigBrother
+{
+    class Program
+    {
+
+        private DiscordSocketClient _client;
+
+        static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient();
+            await _client.LoginAsync(TokenType.Bot, "MzgxMjU5ODQwMjU4OTY1NTA0.DPJDKw.v3b5tHCdy_9BeC7VKfSbQNxEQBc");
+            await _client.StartAsync();
+            await Task.Delay(15000);
+            await _client.StopAsync();
+            await _client.LogoutAsync();
+        }
+
+    }
+}
