e30d85d9b3a9e7b4e58acd07a5776c19ad39e06c
HarpocratesM <crabnebulakek@gmail.com>
2017-11-18 21:34:07 -0800

    Added some simple stuff such as ignoring unknown commands and printing messages from the server (singular)

---
+++ BigBrother/BigBrother/CommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace BigBrother
+{
+    public class CommandHandler
+    {
+
+        private DiscordSocketClient _client;
+        private CommandService _service;
+
+        public CommandHandler(DiscordSocketClient client)
+        {
+            _client = client;
+            _service = new CommandService();
+            _service.AddModulesAsync(Assembly.GetEntryAssembly());
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage s)
+        {
+            var msg = s as SocketUserMessage;
+            if (msg == null) return;
+            Console.WriteLine("[" + msg.CreatedAt + "] " + msg.Author + ": "  + msg.Content);
+            var context = new SocketCommandContext(_client, msg);
+
+            int argPos = 0;
+            if(msg.HasCharPrefix('!', ref argPos))
+            {
+                var result = await _service.ExecuteAsync(context, argPos);
+                if(!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                {
+                    await context.Channel.SendMessageAsync(result.ErrorReason);
+                }
+            }
+        }
+
+    }
+}
--- BigBrother/BigBrother/Program.cs
+++ BigBrother/BigBrother/Program.cs
@@ -9,6 +9,7 @@ namespace BigBrother
     {
 
         private DiscordSocketClient _client;
+        private CommandHandler _handler;
 
         static void Main(string[] args)
         => new Program().StartAsync().GetAwaiter().GetResult();
@@ -18,12 +19,8 @@ namespace BigBrother
             _client = new DiscordSocketClient();
             await _client.LoginAsync(TokenType.Bot, "MzgxMjU5ODQwMjU4OTY1NTA0.DPJDKw.v3b5tHCdy_9BeC7VKfSbQNxEQBc");
             await _client.StartAsync();
-            Console.WriteLine("Logged in and started.");
-            await Task.Delay(15000);
-            Console.WriteLine("Logging out and stopping.");
-            await _client.StopAsync();
-            await _client.LogoutAsync();
-            await Task.Delay(3000);
+            _handler = new CommandHandler(_client);
+            await Task.Delay(-1);
         }
 
     }
